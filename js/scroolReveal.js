0946a8e9a1129717e0a627f7a7fee267a199375e
Jonard Siena <lilkillah.111@gmail.com>
2020-03-03 07:40:58 -0800

    initial-commit

---
+++ js/scroolReveal.js
@@ -0,0 +1,36 @@
+        window.sr = ScrollReveal();
+          sr.reveal('.AboutMe p',{
+          duration:2000,
+          origin:'left',
+          viewFactor:0.2
+          
+        });
+		
+		  sr.reveal('.CanDo i',{
+          duration:2000,
+          origin:'right',
+          viewFactor:0.2
+
+		});
+		  
+		  sr.reveal('.CanDo',{
+          duration:2000,
+          origin:'left',
+          viewFactor:0.2
+
+		});
+		
+		   sr.reveal('.contact',{
+          duration:2000,
+          origin:'bottom',
+          viewFactor:0.2
+
+		});
+
+           sr.reveal('.input-group',{
+          duration:2000,
+          origin:'left',
+          viewFactor:0.2
+
+    });
+
